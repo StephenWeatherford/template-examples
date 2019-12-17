7432dbe0cd8c343ea523eb0b19ac7b912ac6ceec
Stephen Weatherford <stephwe@microsoft.com>
2019-12-17 13:34:38 -0800

    Params with object def value

---
+++ deployRG.bat
@@ -0,0 +1,1 @@
+powershell.exe .\deployRG.ps1 %1 %2
---
+++ deployRG.ps1
@@ -0,0 +1,18 @@
+﻿ param (
+    [Parameter(Mandatory=$true)][string]$templateFile,
+    [string]$paramsFile
+ )
+
+#Connect-AzAccount
+#Set-AzContext -SubscriptionName
+
+$rg = Get-AzResourceGroup deleteme -ErrorAction Ignore
+if (!$rg) {
+   New-AzResourceGroup -Name deleteme -Location westus2
+}
+
+if ($paramsFile) {
+   New-AzResourceGroupDeployment -ResourceGroupName deleteme -TemplateFile $templateFile -TemplateParameterFile $paramsFile
+} else {
+   New-AzResourceGroupDeployment -ResourceGroupName deleteme -TemplateFile $templateFile
+}
