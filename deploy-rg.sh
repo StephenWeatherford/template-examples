ce0aa7a930d179e7ae843830fb2a571b0f69d511
Stephen Weatherford <stephwe@microsoft.com>
2020-06-11 15:57:12 -0700

    more

--- deploy-rg.sh
+++ deploy-rg.sh
@@ -1,5 +1,5 @@
 # Deploy to resource group
-if $2; then
+if [ -z "$2" ]; then
     az group deployment create -g deleteme --handle-extended-json-format --template-file $1
 else
     az group deployment create -g deleteme --handle-extended-json-format --template-file $1 --parameters $2
