adde110cc1edd911e7ef2c30ac0ee14f99ba9bc5
Stephen Weatherford <stephwe@microsoft.com>
2020-06-18 14:56:02 -0700

    changes

---
+++ linkedTemplates/twoLinked/deploy-master1.sh
@@ -0,0 +1,2 @@
+# blocked by https://github.com/Azure/azure-cli/issues/13998: az deployment create --location WestUS --template-file ./master1.json --handle-extended-json-format --parameters @master1.dev.params.json
+az deployment create --location WestUS --template-file ./master1.json
