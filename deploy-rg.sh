fbbf9844f87bef64a1973580e21a0e5b6abff003
Stephen Weatherford <stephwe@microsoft.com>
2020-08-27 12:42:34 -0700

    Update deploy-rg.sh

--- deploy-rg.sh
+++ deploy-rg.sh
@@ -1,6 +1,6 @@
 # Deploy to resource group
 if [ -z "$2" ]; then
-    az group deployment create -g deleteme --handle-extended-json-format --template-file $1
+    az deployment group create -g deleteme --template-file $1
 else
-    az group deployment create -g deleteme --handle-extended-json-format --template-file $1 --parameters $2
+    az deployment group create -g deleteme --template-file $1 --parameters $2
 fi
