9ea689d235942df043e71bbdf1880d639059b080
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-02-11 11:11:25 -0800

    updates

--- linkedTemplates/contentVersion/deploy.sh
+++ linkedTemplates/contentVersion/deploy.sh
@@ -3,4 +3,5 @@ VERSION=${1-master}
 URI=https://raw.githubusercontent.com/StephenWeatherford/template-examples/${VERSION}/linkedTemplates/contentVersion/main.json
 
 echo Deploying: $URI
+set -x
 az deployment group create -g deleteme -u $URI
