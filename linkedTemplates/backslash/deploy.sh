1bfde22dbbbdef8b6494c9e3e255136f05b4c214
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-02-11 12:15:46 -0800

    deploy script

--- linkedTemplates/backslash/deploy.sh
+++ linkedTemplates/backslash/deploy.sh
@@ -2,12 +2,16 @@
 
 STGACCT=sawdeploy
 STGCONTAINER=sawdeploy
-az storage blob sync -c $STGCONTAINER --account-name $STGACCT -s .
+TEMPLATE=bs.json
+PARAMS=bs.params.json
+RG=deleteme
 
-BASEURI=https://sawdeploy.blob.core.windows.net/sawdeploy
-SAS="?sv=2019-12-12&si=sawdeploy&sr=c&sig=SzLJFWrlZqbeKTCb4UXtPWuO64yu9Fv2McMjpX%2FqG9A%3D"
-URI=${BASEURI}/bs.json${SAS}
+az storage blob sync -c $STGCONTAINER --account-name $STGACCT -s ./templates
+
+BASEURI=https://sawdeploy.blob.core.windows.net/${STGCONTAINER}
+SAS='sv=2019-12-12&si=sawdeploy&sr=c&sig=SzLJFWrlZqbeKTCb4UXtPWuO64yu9Fv2McMjpX%2FqG9A%3D'
+URI=${BASEURI}/${TEMPLATE}
 
 echo Deploying: $URI
 set -x
-az deployment group create -g deleteme -u $URI
+az deployment group create -g ${RG} -u ${URI} -q ${SAS} -p ${PARAMS}
