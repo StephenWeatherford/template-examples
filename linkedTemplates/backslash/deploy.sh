931c4c02d33e32ad725bb9cb3fb96fd2474775dd
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-02-10 17:39:55 -0800

    update

--- linkedTemplates/backslash/deploy.sh
+++ linkedTemplates/backslash/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-VERSION=${1-master}
-URI=https://raw.githubusercontent.com/StephenWeatherford/template-examples/${VERSION}/linkedTemplates/backslash/bs.json
+
+STGACCT=sawdeploy
+STGCONTAINER=sawdeploy
+az storage blob sync -c $STGCONTAINER --account-name $STGACCT -s .
+
+BASEURI=https://sawdeploy.blob.core.windows.net/sawdeploy
+SAS="?sv=2019-12-12&si=sawdeploy&sr=c&sig=SzLJFWrlZqbeKTCb4UXtPWuO64yu9Fv2McMjpX%2FqG9A%3D"
+URI=${BASEURI}/bs.json${SAS}
 
 echo Deploying: $URI
 set -x
