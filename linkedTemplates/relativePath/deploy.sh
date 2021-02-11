4583df02b51ddf5684b4746927711e9640344b16
Stephen Weatherford <stephen.weatherford@microsoft.com>
2021-02-11 11:30:29 -0800

    relativePath fixes

--- linkedTemplates/relativePath/deploy.sh
+++ linkedTemplates/relativePath/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-VERSION=${1-master}
-URI=https://raw.githubusercontent.com/StephenWeatherford/template-examples/${VERSION}/linkedTemplates/relativePath/main.json
+
+STGACCT=sawdeploy
+STGCONTAINER=sawdeploy
+az storage blob sync -c $STGCONTAINER --account-name $STGACCT -s .
+
+BASEURI=https://sawdeploy.blob.core.windows.net/sawdeploy
+SAS="?sv=2019-12-12&si=sawdeploy&sr=c&sig=SzLJFWrlZqbeKTCb4UXtPWuO64yu9Fv2McMjpX%2FqG9A%3D"
+URI=${BASEURI}/main.json${SAS}
 
 echo Deploying: $URI
-az deployment group create -g deleteme -u $URI
+set -x
+az deployment group create -g deleteme -u $URI -p main.parameters.json
